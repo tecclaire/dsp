02d42e2fd882268c4f54ef9938b3ccc26ad79d56
Claire <30413557+tecclaire@users.noreply.github.com>
2017-09-07 17:21:11 -0400

    Solved Exercise q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -4,10 +4,36 @@
 date_start = '01-02-2013'  
 date_stop = '07-28-2015'   
 
+from datetime import datetime
+
+stt = datetime.strptime(date_start, '%m-%d-%Y')
+stp = datetime.strptime(date_stop, '%m-%d-%Y')
+
+d = stp - stt
+
+print (d.days)
+
+
 ####b)  
 date_start = '12312013'  
 date_stop = '05282015'  
 
+from datetime import datetime
+stt = datetime.strptime(date_start, '%m%d%Y')
+stp = datetime.strptime(date_stop, '%m%d%Y')
+
+d = stp - stt
+
+print (d.days)
+
 ####c)  
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
+
+from datetime import datetime
+stt = datetime.strptime(date_start, '%d-%b-%Y')
+stp = datetime.strptime(date_stop, '%d-%b-%Y')
+
+d = stp - stt
+
+print (d.days)
